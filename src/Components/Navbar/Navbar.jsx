3673a274be8ee4781316837deb52c6d209f9100d
SreelekhaaNagamalliSanthoshkumar <ssree22299@gmail.com>
2025-11-09 22:55:07 -0600

    Update connect me button

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,15 @@ import { RiMenu3Line } from 'react-icons/ri'
 import { IoClose } from 'react-icons/io5'
 
 const Navbar = () => {
-  
-  const [menu,setMenu] = useState("home");
+
+  const [menu, setMenu] = useState("home");
   const menuRef = useRef();
 
   const openMenu = () => {
-    menuRef.current.style.right="0";
+    menuRef.current.style.right = "0";
   }
   const closeMenu = () => {
-    menuRef.current.style.right="-300px";
+    menuRef.current.style.right = "-300px";
   }
 
   return (
@@ -22,15 +22,15 @@ const Navbar = () => {
       <RiMenu3Line onClick={openMenu} className='nav-mob-open' />
       <ul ref={menuRef} className="nav-menu">
         <IoClose onClick={closeMenu} className="nav-mob-close" />
-        <li><AnchorLink className='anchor-link' href='#home' offset={100}><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home"?<img src={underline} alt=''/>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink>{menu==="about"?<img src={underline} alt=''/>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#experience'><p onClick={()=>setMenu("experience")}>Experience</p></AnchorLink>{menu==="experience"?<img src={underline} alt=''/>:<></>}</li>
-        <li><AnchorLink className='anchor-link' offset={50} href='#education'><p onClick={()=>setMenu("education")}>Education</p></AnchorLink>{menu==="education"?<img src={underline} alt=''/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' href='#home' offset={100}><p onClick={() => setMenu("home")}>Home</p></AnchorLink>{menu === "home" ? <img src={underline} alt='' /> : <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={() => setMenu("about")}>About Me</p></AnchorLink>{menu === "about" ? <img src={underline} alt='' /> : <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#experience'><p onClick={() => setMenu("experience")}>Experience</p></AnchorLink>{menu === "experience" ? <img src={underline} alt='' /> : <></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#education'><p onClick={() => setMenu("education")}>Education</p></AnchorLink>{menu === "education" ? <img src={underline} alt='' /> : <></>}</li>
 
         {/* <li><AnchorLink className='anchor-link' offset={50} href='#work'><p onClick={()=>setMenu("work")}>Portfolio</p></AnchorLink>{menu==="work"?<img src={underline} alt=''/>:<></>}</li> */}
-        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact"?<img src={underline} alt=''/>:<></>}</li>
+        <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={() => setMenu("contact")}>Contact</p></AnchorLink>{menu === "contact" ? <img src={underline} alt='' /> : <></>}</li>
       </ul>
-      <div className="nav-connect"><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
+      <div className="nav-connect" onClick={() => window.location.href = '#contact'}><AnchorLink className='anchor-link' offset={50} href='#contact'>Connect With Me</AnchorLink></div>
     </div>
   )
 }
